3e526e9885a9abaa2f1baa34bb61bacc0de5fd8a
bjzwjsp <15005182358@163.com>
2023-04-08 22:09:27 +0800

    测试1

---
+++ src/components/Modal/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mask">
+    ssss
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {
+        };
+    },
+
+    components: {},
+
+    computed: {},
+
+    mounted: {},
+
+    methods: {}
+}
+
+</script>
+<style scoped>
+.mask {
+
+}
+</style>
